6c6d61a5a47b17278217590bae6dd70bb44e2048
The Doctor <learicist@gmail.com>
2023-01-08 03:32:28 -0500

    creation of build page, styling, skeleton laid out. work on navbar, attempt to bug solve for collapse menu, styling menu to right side. color adjustments, border kills, temp pic imports, class generation. 5 hours.

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 console.log("First Load of Page");
+
+$(document).ready(function () {
+
+    $('#titleLogo').on('click', function () {
+        location.reload();
+    });
+
+
+    $('#home').on('click', function () {
+        $('#pageOne').removeClass('hidden');
+        $('#buildPage').addClass('hidden');
+    });
+
+    $('#constBtn').on('click', function () {
+        $('#pageOne').addClass('hidden');
+        $('#buildPage').removeClass('hidden');
+    });
+});
